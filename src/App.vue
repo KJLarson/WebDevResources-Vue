80bb93542c20cb29f50d8638e85443415d474e26
Kari <42503401+KJLarson@users.noreply.github.com>
2019-10-04 12:08:07 -0500

    Move text-align from #app to h1

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,10 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1 {
+  text-align: center;
 }
 </style>
